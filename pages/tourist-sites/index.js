551f465212485b4641c2d94519f222756da7559f
braincee <kwesi938@gmail.com>
2023-01-30 23:07:38 +0000

    Add images of regions

---
+++ pages/tourist-sites/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+        <h1>Tourist Site Pages</h1>
+        <div>
+        <a href="/">
+            <img />
+            <h2>Tourist Sites in Eastern Region</h2>
+         </a>
+         <a href="/">
+            <img />
+            <h2>Tourist Sites in Central Region</h2>
+         </a>
+         <a href="/">
+            <img />
+            <h2>Tourist Sites in Ashanti Region</h2>
+         </a>
+        </div>
+    </div>
+  )
+}
+
+export default Events
