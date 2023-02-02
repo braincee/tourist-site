6bf859fa21a1e047090105f0b27cb6278e5b3312
braincee <kwesi938@gmail.com>
2023-02-02 19:45:25 +0000

    render more data to the home page component

--- src/components/home/home-page.js
+++ src/components/home/home-page.js
@@ -3,15 +3,24 @@ import Image from "next/image";
 
 const HomePage = ({data}) => {
     return (
-        <>
+        <div className="home-body">
+          <h2>REGIONS IN GHANA</h2>
         {data.map((t) => (
-          <Link key={t.id} href={`/tourist-sites/${t.id}`} passHref>
-            <Image width={200} height={200} alt={t.id} src={t.image}/>
-            <h2>{t.title}</h2><p>{t.description}</p>
+          <Link key={t.id} href={`/tourist-sites/${t.id}`} passHref className="home-card">
+            <div className="image"><Image width={500} height={300} alt={t.id} src={t.image}/></div>
+            <div><h2>{t.title}</h2><p className="description">{t.description}</p>
+            <ul className="list">
+                <li>Area: {t.area}</li>
+                <li>Captial: {t.capital}</li>
+                <li>District: {t.district}</li>
+                <li>Population: {t.population}</li>
+                <li>Language: {t.language}</li>
+               </ul>
+            </div>
           </Link>
     
         ))}
-      </>
+      </div>
     );
 }
 
