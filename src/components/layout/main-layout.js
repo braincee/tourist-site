d39914a7e169d096a7d4449dd42d1e05f6f9f599
braincee <kwesi938@gmail.com>
2023-02-03 21:53:18 +0000

    edit styling for layout page

--- src/components/layout/main-layout.js
+++ src/components/layout/main-layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from '../header/header'
 import Footer from '../footer/footer'
+import Navbar from '../header/header'
 
 const Layout = ({ children }) => {
   return (
     <>
-     <Header />
-      <main>{children}</main>
+     <Navbar />
+      {children}
      <Footer />
     </>
   )
