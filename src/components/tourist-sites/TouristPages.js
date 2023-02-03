0c36a4328b107856b3db22d181f5e67feae7ed86
braincee <kwesi938@gmail.com>
2023-02-03 21:53:53 +0000

    edit styling for tourist pages

--- src/components/tourist-sites/TouristPages.js
+++ src/components/tourist-sites/TouristPages.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const TouristPages = ({data}) => {
   return (
-    <div className='tourist-pages'>
+    <div className='main-body tourist-pages'>
       <h2>Cities in Ghana</h2>
         {data.map((t) => (
             <Link key={t.id} href={`/tourist-sites/${t.id}`} passHref className='card'>
-                <div className='image'><Image src={t.image} alt={t.image} width={500} height={350}/></div>
+                <div><Image src={t.image} alt={t.image} width={500} height={350}/></div>
                 <div><h2>{t.title}</h2><p className='description'>{t.description}</p>
                 <ul className='list'>
                    <li>District: {t.district}</li>
