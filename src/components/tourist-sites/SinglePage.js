93132d77ca779760fc91cd67305958e8906019ff
braincee <kwesi938@gmail.com>
2023-02-07 00:36:27 +0000

    node--

--- src/components/tourist-sites/SinglePage.js
+++ src/components/tourist-sites/SinglePage.js
@@ -3,14 +3,11 @@ import Image from 'next/image'
 
 
 const SinglePage = ({ data }) => {
-    const onSubmit = () => {
-
-    };
-
+   
   return (
     <div className='single-page'>
         <div>
-          <Image src={data.image} width={700} height={400} alt={data.title}/>
+          <Image src={data.image} width={700} height={400} alt={data.title} />
         </div>
         <div>
           <h2>{data.title}</h2>
