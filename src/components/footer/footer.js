c15b0854fde2949521a15a1d91623fc40244e45a
braincee <kwesi938@gmail.com>
2023-02-08 13:14:49 +0000

    update the footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <p> &#169; 2023 Built by Stephen Annor with Next JS</p>
+        <p> &#169; 2023 Built with Next JS by Stephen Annor </p>
     </footer>
   )
 }
